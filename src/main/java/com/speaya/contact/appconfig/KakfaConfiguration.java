0589e68dcf5732c444caafcbc1017b543feb5d07
Usman <usmanali4073@gmail.com>
2019-09-03 18:23:27 -0400

    Kafka Consumer is work As Will using the Local Settings

---
+++ src/main/java/com/speaya/contact/appconfig/KakfaConfiguration.java
@@ -0,0 +1,65 @@
+package com.speaya.contact.appconfig;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KakfaConfiguration {
+
+    //Config for Producer
+    public Map<String, Object> configProducer(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return  config;
+    }
+
+    //Producer Code
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(configProducer());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
+
+    //Consumer
+    //Config for for Consumer
+    public Map<String, Object> configConsumer(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return  config;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(configConsumer());
+    }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+}
--- src/main/java/com/speaya/contact/service/KafkaService.java
+++ src/main/java/com/speaya/contact/service/KafkaService.java
@@ -1,6 +1,7 @@
 package com.speaya.contact.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,10 @@ public class KafkaService {
 
     public void send(String message) {
         this.kafkaTemplate.send(KafkaTopic, message);
+    }
+
+    @KafkaListener(topics = "javainuse-topic", groupId = "group_id")
+    public void consume(String message){
+        System.out.println(message);
     }
 }
